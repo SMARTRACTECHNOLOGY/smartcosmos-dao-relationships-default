607c03548bd4ccdb938fc8271c85ce204a0e9c82
Andreas Siegel <andreas.siegel@smartrac-group.com>
2016-06-02 17:51:36 +0200

    Add findAllReflexive test

--- src/test/java/net/smartcosmos/dao/relationships/impl/RelationshipPersistanceServiceTest.java
+++ src/test/java/net/smartcosmos/dao/relationships/impl/RelationshipPersistanceServiceTest.java
@@ -27,6 +27,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 import static org.junit.Assert.*;
 
@@ -317,5 +318,69 @@ public class RelationshipPersistanceServiceTest {
         assertEquals(TEST_REFERENCE_TYPE, responseList.get(0).getRelatedEntityReferenceType());
         assertEquals(TEST_RELATED_ENTITY, responseList.get(0).getRelatedReferenceUrn());
         assertEquals(TEST_MONIKER, responseList.get(0).getMoniker());
+    }
+
+    @Test
+    public void testFindAllReflexive() {
+        final String ENTITY_URN_A = "urn:uuid:" + UuidUtil.getNewUuidAsString();
+        final String ENTITY_URN_B = "urn:uuid:" + UuidUtil.getNewUuidAsString();
+
+        final String REFERENCE_TYPE = "Thing";
+        final String RELATIONSHIP_TYPE_REFLEXIVE = "Reflexive";
+        final String RELATIONSHIP_TYPE_NON_REFLEXIVE_A = "Not Reflexive";
+        final String RELATIONSHIP_TYPE_NON_REFLEXIVE_B = "Not Reflexive as well";
+
+        RelationshipUpsert reflexiveEntity1 = RelationshipUpsert.builder()
+            .entityReferenceType(REFERENCE_TYPE)
+            .referenceUrn(ENTITY_URN_A)
+            .type(RELATIONSHIP_TYPE_REFLEXIVE)
+            .relatedEntityReferenceType(REFERENCE_TYPE)
+            .relatedReferenceUrn(ENTITY_URN_B)
+            .build();
+        final String reflexiveUrn1 = relationshipPersistenceService.upsert(accountUrn, reflexiveEntity1).getUrn();
+
+        RelationshipUpsert reflexiveEntity2 = RelationshipUpsert.builder()
+            .entityReferenceType(REFERENCE_TYPE)
+            .referenceUrn(ENTITY_URN_B)
+            .type(RELATIONSHIP_TYPE_REFLEXIVE)
+            .relatedEntityReferenceType(REFERENCE_TYPE)
+            .relatedReferenceUrn(ENTITY_URN_A)
+            .build();
+        final String reflexiveUrn2 = relationshipPersistenceService.upsert(accountUrn, reflexiveEntity2).getUrn();
+
+        RelationshipUpsert nonReflexiveEntity1 = RelationshipUpsert.builder()
+            .entityReferenceType(REFERENCE_TYPE)
+            .referenceUrn(ENTITY_URN_A)
+            .type(RELATIONSHIP_TYPE_NON_REFLEXIVE_A)
+            .relatedEntityReferenceType(REFERENCE_TYPE)
+            .relatedReferenceUrn(ENTITY_URN_B)
+            .build();
+        final String nonReflexiveUrn1 = relationshipPersistenceService.upsert(accountUrn, nonReflexiveEntity1).getUrn();
+
+        RelationshipUpsert nonReflexiveEntity2 = RelationshipUpsert.builder()
+            .entityReferenceType(REFERENCE_TYPE)
+            .referenceUrn(ENTITY_URN_B)
+            .type(RELATIONSHIP_TYPE_NON_REFLEXIVE_B)
+            .relatedEntityReferenceType(REFERENCE_TYPE)
+            .relatedReferenceUrn(ENTITY_URN_A)
+            .build();
+        final String nonReflexiveUrn2 = relationshipPersistenceService.upsert(accountUrn, nonReflexiveEntity2).getUrn();
+
+        List<RelationshipResponse> responseList = relationshipPersistenceService.findAllReflexive(accountUrn, REFERENCE_TYPE, ENTITY_URN_A);
+
+        assertFalse(responseList.isEmpty());
+        assertEquals(2, responseList.size());
+        assertEquals(RELATIONSHIP_TYPE_REFLEXIVE, responseList.get(0).getType());
+        assertEquals(RELATIONSHIP_TYPE_REFLEXIVE, responseList.get(1).getType());
+
+        List<UUID> uuidList = responseList.stream()
+            .map(o -> UuidUtil.getUuidFromUrn(o.getUrn()))
+            .collect(Collectors.toList());
+
+        assertTrue(uuidList.contains(reflexiveUrn1));
+        assertTrue(uuidList.contains(reflexiveUrn2));
+
+        assertFalse(uuidList.contains(nonReflexiveUrn1));
+        assertFalse(uuidList.contains(nonReflexiveUrn2));
     }
 }
