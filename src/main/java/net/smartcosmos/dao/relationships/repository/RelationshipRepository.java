6e052d6d0a8e4a99a5b809e789f11b0c82d378ce
Andreas Siegel <andreas.siegel@smartrac-group.com>
2016-06-02 14:39:11 +0200

    Fix method names

--- src/main/java/net/smartcosmos/dao/relationships/repository/RelationshipRepository.java
+++ src/main/java/net/smartcosmos/dao/relationships/repository/RelationshipRepository.java
@@ -26,9 +26,9 @@ public interface RelationshipRepository extends
         String relatedEntityReferenceType,
         UUID relatedReferenceId);
 
-    List<RelationshipEntity> findByAccountIdAndEntityReferenceTypeAndReferenceUrnAndType(UUID accountId, String entityReferenceType, UUID referenceId, String type);
+    List<RelationshipEntity> findByAccountIdAndEntityReferenceTypeAndReferenceIdAndType(UUID accountId, String entityReferenceType, UUID referenceId, String type);
 
-    List<RelationshipEntity> findByAccountIdAndRelatedEntityReferenceTypeAndRelatedReferenceUrnAndType(UUID accountId, String relatedEntityReferenceType, UUID relatedReferenceId, String type);
+    List<RelationshipEntity> findByAccountIdAndRelatedEntityReferenceTypeAndRelatedReferenceIdAndType(UUID accountId, String relatedEntityReferenceType, UUID relatedReferenceId, String type);
 
     @Transactional
     List<RelationshipEntity> deleteByAccountIdAndId(UUID accountId, UUID id);
